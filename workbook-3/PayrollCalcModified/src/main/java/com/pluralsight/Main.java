df737ddaef97111e052307e9733c2c8eeae93e9c
sharad101 <sharadpatel801@gmail.com>
2025-04-23 16:02:36 -0400

    today's update

---
+++ workbook-3/PayrollCalcModified/src/main/java/com/pluralsight/Main.java
@@ -0,0 +1,30 @@
+package com.pluralsight;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the name of the employee and the process: ");
+        String inputFilename = scanner.nextLine();
+
+        System.out.println("Enter the name of the payroll file");
+        String outputFilename = scanner.nextLine();
+
+        try{
+
+
+
+
+
+
+
+
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+}
